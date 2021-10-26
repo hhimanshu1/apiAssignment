8076d494e8427f5e5c7528317ca050cfb8c7d9e0
Himanshu Hemant <himanshuhemant1512@gmail.com>
2021-10-26 23:18:30 +0530

    view

--- src/components/ViewUser.js
+++ src/components/ViewUser.js
@@ -9,7 +9,6 @@ const ViewUser = () => {
     name: "",
     username: "",
     email: "",
-    address: "",
     website: "",
     phone: "",
   });
@@ -24,31 +23,32 @@ const ViewUser = () => {
   return (
     <>
       <div>
-      <div class="bod">
-    <div class="many"> 
-        <FormGroup id="viewm">
-          <Label for="exampleUserName">Username: {user.username} </Label>
-        </FormGroup>
-        <FormGroup >
-          <Label for="exampleName">Name: {user.name}</Label>
-        </FormGroup>
-        <FormGroup id="viewm">
-          <Label for="exampleEmail">Email: {user.email}</Label>
-        </FormGroup>
-        <FormGroup >
-          <Label for="exampleCity">City: {user.address.city}</Label>
-        </FormGroup>
-        <FormGroup id="viewm">
-          <Label for="exampleName">Phone: {user.phone}</Label>
-        </FormGroup>
-        <FormGroup >
-          <Label for="exampleWeb">WebSite: {user.website}</Label>
-        </FormGroup>
-      </div>
-      <span id="btn1">
-      <Link to="/"> <span id="sp1">Back to Home </span></Link>
-       </span>
-      </div>
+        <div class="bod">
+          <div class="many">
+            <FormGroup id="viewm">
+              <Label for="exampleUserName">Username: {user.username} </Label>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleName">Name: {user.name}</Label>
+            </FormGroup>
+            <FormGroup id="viewm">
+              <Label for="exampleEmail">Email: {user.email}</Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="exampleName">Phone: {user.phone}</Label>
+            </FormGroup>
+            <FormGroup id="viewm">
+              <Label for="exampleWeb">WebSite: {user.website}</Label>
+            </FormGroup>
+          </div>
+          <span id="btn1">
+            <Link to="/">
+              {" "}
+              <span id="sp1">Back to Home </span>
+            </Link>
+          </span>
+        </div>
       </div>
     </>
   );
